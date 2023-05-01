1666e3bd615ba1fb4489296f16d7ef51a0a47d61
Phoenix Deinert <phoenix.deinert@gmail.com>
2023-04-30 22:20:38 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-function addingEventListener() {
-}
+document.addEventListener('DOMContentLoaded', function() {
+    // Code to bind event listener
+  });
+  
+  function addingEventListener() {
+    document.addEventListener('DOMContentLoaded', function() {
+      const button = document.querySelector('#input');
+  
+      function clickAlert() {
+        alert('Button clicked!');
+      }
+  
+      button.addEventListener('click', clickAlert);
+    });
+  }
+  
+  addingEventListener();
+   
